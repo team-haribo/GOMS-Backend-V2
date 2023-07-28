019a085ea31b1dd6a76246e214bcd35f56e74619
yoontaebin <gusckd000@gmail.com>
2023-07-28 14:15:08 +0900

    :recycle: :: HttpMessageNotReadableException handler 삭제

--- goms-infrastructure/src/main/kotlin/com/goms/v2/global/exception/handler/GlobalExceptionHandler.kt
+++ goms-infrastructure/src/main/kotlin/com/goms/v2/global/exception/handler/GlobalExceptionHandler.kt
@@ -4,7 +4,6 @@ import com.goms.v2.global.exception.exception.GomsException
 import com.goms.v2.global.exception.response.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -21,14 +20,6 @@ class GlobalExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException::class)
 	fun  handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> =
-		ResponseEntity(
-			ErrorResponse(e.message, e.hashCode()),
-			HttpStatus.valueOf(e.hashCode())
-		)
-
-
-	@ExceptionHandler(HttpMessageNotReadableException::class)
-	fun  handleHttpMessageNotReadableException(e: HttpMessageNotReadableException): ResponseEntity<ErrorResponse> =
 		ResponseEntity(
 			ErrorResponse(e.message, e.hashCode()),
 			HttpStatus.valueOf(e.hashCode())
