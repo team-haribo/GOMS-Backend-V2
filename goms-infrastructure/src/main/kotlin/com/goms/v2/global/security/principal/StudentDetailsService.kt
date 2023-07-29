f1f0275f4fe31427a85326cd9d42c7be8f8b4f46
KimGyeongsuuu <sk62628504@gmail.com>
2023-07-29 10:44:15 +0900

    :sparkles: :: StudentDetailsService 추가

---
+++ goms-infrastructure/src/main/kotlin/com/goms/v2/global/security/principal/StudentDetailsService.kt
@@ -0,0 +1,22 @@
+package com.goms.v2.global.security.principal
+
+import com.goms.v2.global.security.jwt.common.exception.InvalidTokenException
+import com.goms.v2.repository.AccountRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+@Service
+class StudentDetailsService(
+    private val accountRepository: AccountRepository
+): UserDetailsService {
+
+    @Transactional(readOnly = true, rollbackFor = [Exception::class])
+    override fun loadUserByUsername(username: String?): UserDetails =
+        accountRepository.findByIdOrNull(UUID.fromString(username))
+            .let { it ?: throw InvalidTokenException() }
+            .let { StudentDetails(it.idx) }
+
+}
