612caeb4d6a0551f015988d031531aaeb6576317
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-03 22:23:10 +0900

    :sparkles: :: default 메서드로 converter 구현하기

--- goms-presentation/src/main/kotlin/com/goms/v2/domain/auth/mapper/AuthDataMapper.kt
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/auth/mapper/AuthDataMapper.kt
@@ -2,7 +2,10 @@ package com.goms.v2.domain.auth.mapper
 
 import com.goms.v2.domain.auth.dto.request.SignInDto
 import com.goms.v2.domain.auth.dto.request.SignInHttpRequest
+import com.goms.v2.domain.auth.dto.response.TokenDto
+import com.goms.v2.domain.auth.dto.response.TokenHttpResponse
 import org.mapstruct.*
+import java.time.LocalDateTime
 
 @Mapper(
     componentModel = MappingConstants.ComponentModel.SPRING,
@@ -12,5 +15,14 @@ import org.mapstruct.*
 interface AuthDataMapper {
 
     fun toDto(signInHttpRequest: SignInHttpRequest?): SignInDto
+
+    fun toResponse(tokeInDto: TokenDto): TokenHttpResponse =
+        TokenHttpResponse(
+            accessToken = tokeInDto.accessToken,
+            refreshToken = tokeInDto.refreshToken,
+            accessTokenExp = LocalDateTime.now().plusSeconds(tokeInDto.accessTokenExp.toLong()),
+            refreshTokenExp = LocalDateTime.now().plusSeconds(tokeInDto.refreshTokenExp.toLong()),
+            authority = tokeInDto.authority
+        )
 
 }
