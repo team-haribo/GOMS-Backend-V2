ac689cb79592e3231543548ee7d176b3a0d124c6
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-03 15:38:40 +0900

    :sparkles: :: 예외처리 추가

--- goms-infrastructure/src/main/kotlin/com/goms/v2/thirdparty/gauth/GAuthAdapter.kt
+++ goms-infrastructure/src/main/kotlin/com/goms/v2/thirdparty/gauth/GAuthAdapter.kt
@@ -3,6 +3,9 @@ package com.goms.v2.thirdparty.gauth
 import com.goms.v2.domain.auth.dto.GAuthTokenDto
 import com.goms.v2.domain.auth.dto.GAuthUserInfoDto
 import com.goms.v2.domain.auth.exception.*
+import com.goms.v2.domain.auth.exception.gauth.ExpiredGAuthCodeException
+import com.goms.v2.domain.auth.exception.gauth.GAuthSecretMismatchException
+import com.goms.v2.domain.auth.exception.gauth.GAuthServiceNotFoundException
 import com.goms.v2.domain.auth.spi.GAuthPort
 import com.goms.v2.gloabl.exception.exception.GomsException
 import com.goms.v2.thirdparty.gauth.property.GAuthProperties
@@ -43,9 +46,9 @@ class GAuthAdapter(
 
     private fun gAuthExceptionHandler(error: GAuthException): GomsException {
         return when (error.code) {
-            400 -> SecretMismatchException()
-            401 -> ExpiredCodeException()
-            404 -> ServiceNotFoundException()
+            400 -> GAuthSecretMismatchException()
+            401 -> ExpiredGAuthCodeException()
+            404 -> GAuthServiceNotFoundException()
             else -> InternalServerErrorException()
         }
     }
