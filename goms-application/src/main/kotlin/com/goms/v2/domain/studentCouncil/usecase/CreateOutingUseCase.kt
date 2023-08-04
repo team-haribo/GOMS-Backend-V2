2c03c1c376cea71d5002685f0c584d667f40e3f0
yoontaebin <gusckd000@gmail.com>
2023-08-05 01:46:45 +0900

    :sparkles: :: CreateOutingUseCase 추가

---
+++ goms-application/src/main/kotlin/com/goms/v2/domain/studentCouncil/usecase/CreateOutingUseCase.kt
@@ -0,0 +1,24 @@
+package com.goms.v2.domain.studentCouncil.usecase
+
+import com.goms.v2.common.annotation.UseCaseWithTransaction
+import com.goms.v2.common.property.studentCouncil.OutingUUIDExpTimeProperties
+import com.goms.v2.domain.studentCouncil.OutingUUID
+import com.goms.v2.repository.studentCouncil.OutingUUIDRepository
+import java.util.*
+
+@UseCaseWithTransaction
+class CreateOutingUseCase(
+	private val outingUUIDRepository: OutingUUIDRepository,
+	private val outingUUIDExpTimeProperties: OutingUUIDExpTimeProperties
+) {
+
+	fun execute(): UUID {
+		val outingUUID = OutingUUID(
+			outingUUID = UUID.randomUUID(),
+			expiredAt = outingUUIDExpTimeProperties.expiredAt
+		)
+
+		return outingUUIDRepository.save(outingUUID)
+	}
+
+}
