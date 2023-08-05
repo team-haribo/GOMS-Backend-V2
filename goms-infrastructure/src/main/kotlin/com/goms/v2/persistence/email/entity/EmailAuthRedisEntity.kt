61503aa55a0044062b3a0df02ca615ca0309d6f6
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-05 17:15:26 +0900

    :sparkles: :: EmailAuthRedisEntity 추가

---
+++ goms-infrastructure/src/main/kotlin/com/goms/v2/persistence/email/entity/EmailAuthRedisEntity.kt
@@ -0,0 +1,16 @@
+package com.goms.v2.persistence.email.entity
+
+import org.hibernate.annotations.ColumnDefault
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+
+@RedisHash("email_auth")
+data class EmailAuthRedisEntity(
+    @Id
+    val email: String,
+    val randomValue: String,
+    val authentication: Boolean,
+    @ColumnDefault("0")
+    val attemptCount: Int,
+    val expiredAt: Int
+)
