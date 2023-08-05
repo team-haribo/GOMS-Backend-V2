203fdc8afb7ba91b43aad373079f18e13ae16c78
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-05 19:55:12 +0900

    :recycle: :: . 붙이기

--- goms-application/src/main/kotlin/com/goms/v2/gloabl/exception/ErrorCode.kt
+++ goms-application/src/main/kotlin/com/goms/v2/gloabl/exception/ErrorCode.kt
@@ -16,12 +16,12 @@ enum class ErrorCode(
 	GAUTH_SERVICE_NOT_FOUND("서비스를 찾지 못했습니다.", HttpStatus.NOT_FOUND),
 
 	// EMAIL
-	DUPLICATE_EMAIL("중복된 이메일 입니다", HttpStatus.CONFLICT),
-	MANY_REQUEST_EMAIL("이메일 요청이 5번을 초과했습니다", HttpStatus.TOO_MANY_REQUESTS),
-	MISMATCH_AUTH_KEY("인증번호가 일치하지 않습니다", HttpStatus.BAD_REQUEST),
-	EMAIL_NOT_FOUND("존재하지 않은 이메일 입니다", HttpStatus.NOT_FOUND),
-	NOT_VERIFY_EMAIL("인증되지 않은 이메일 입니다", HttpStatus.UNAUTHORIZED),
-	MAIL_SEND_FAIL("이메일 요청에 실패하셨습니다", HttpStatus.BAD_REQUEST),
+	DUPLICATE_EMAIL("중복된 이메일 입니다.", HttpStatus.CONFLICT),
+	MANY_REQUEST_EMAIL("이메일 요청이 5번을 초과했습니다.", HttpStatus.TOO_MANY_REQUESTS),
+	MISMATCH_AUTH_KEY("인증번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
+	EMAIL_NOT_FOUND("존재하지 않은 이메일 입니다.", HttpStatus.NOT_FOUND),
+	NOT_VERIFY_EMAIL("인증되지 않은 이메일 입니다.", HttpStatus.UNAUTHORIZED),
+	MAIL_SEND_FAIL("이메일 요청에 실패하셨습니다.", HttpStatus.BAD_REQUEST),
 
 	// ACCOUNT
 	ACCOUNT_NOT_FOUND("계정을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
