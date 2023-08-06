7e3aab02938fef1fbf2d996227c20085074e76ca
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-06 11:56:23 +0900

    :sparkles: :: Authentication 애그리거트 추가

--- goms-domain/src/main/kotlin/com/goms/v2/domain/email/Authentication.kt
+++ goms-domain/src/main/kotlin/com/goms/v2/domain/email/Authentication.kt
@@ -3,20 +3,15 @@ package com.goms.v2.domain.email
 import com.goms.v2.common.annotation.RootAggregate
 
 @RootAggregate
-data class EmailAuth(
+data class Authentication(
     val email: String,
-    var authCode: String,
-    var isAuthentication: Boolean,
     var attemptCount: Int,
+    var isAuthentication: Boolean,
     val expiredAt: Int
 ) {
 
     fun updateAuthentication(isAuthentication: Boolean) {
         this.isAuthentication = isAuthentication
-    }
-
-    fun updateAuthKey(authCode: String) {
-        this.authCode = authCode
     }
 
     fun increaseAttemptCount() {
