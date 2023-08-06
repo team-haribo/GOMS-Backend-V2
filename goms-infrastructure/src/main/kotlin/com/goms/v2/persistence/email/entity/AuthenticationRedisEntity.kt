ca93dbed5bacea154a51cee12a89ac40272cb64f
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-06 11:56:43 +0900

    :sparkles: :: AuthenticationRedisEntity 추가

---
+++ goms-infrastructure/src/main/kotlin/com/goms/v2/persistence/email/entity/AuthenticationRedisEntity.kt
@@ -0,0 +1,16 @@
+package com.goms.v2.persistence.email.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+import java.util.concurrent.TimeUnit
+
+@RedisHash("authentication")
+data class AuthenticationRedisEntity(
+    @Id
+    val email: String,
+    val attemptCount: Int,
+    val isAuthentication: Boolean,
+    @TimeToLive(unit = TimeUnit.SECONDS)
+    val expiredAt: Int
+)
