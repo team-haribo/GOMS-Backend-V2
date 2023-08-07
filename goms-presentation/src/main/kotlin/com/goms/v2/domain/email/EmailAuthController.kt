a61218344943b29aa6bb69b086f0283396f64ab0
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-07 14:41:11 +0900

    :sparkles: :: 인증번호 검증 api

--- goms-presentation/src/main/kotlin/com/goms/v2/domain/email/EmailAuthController.kt
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/email/EmailAuthController.kt
@@ -3,23 +3,32 @@ package com.goms.v2.domain.email
 import com.goms.v2.domain.email.dto.request.SendEmailHttpRequest
 import com.goms.v2.domain.email.mapper.EmailAuthDataMapper
 import com.goms.v2.domain.email.usecase.SendEmailUseCase
+import com.goms.v2.domain.email.usecase.VerifyAuthCodeUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/v1/email")
 class EmailAuthController(
     private val emailAuthDataMapper: EmailAuthDataMapper,
-    private val sendEmailUseCase: SendEmailUseCase
+    private val sendEmailUseCase: SendEmailUseCase,
+    private val verifyAuthCodeUseCase: VerifyAuthCodeUseCase
 ) {
 
     @PostMapping("send")
     fun sendEmail(@RequestBody sendEmailHttpRequest: SendEmailHttpRequest): ResponseEntity<Void> =
         sendEmailUseCase.execute(emailAuthDataMapper.toDto(sendEmailHttpRequest))
             .let { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
+
+    @GetMapping("verify")
+    fun verifyAuthCode(@RequestParam email: String,@RequestParam authCode: String): ResponseEntity<Void> =
+        verifyAuthCodeUseCase.execute(email, authCode)
+            .let { ResponseEntity.status(HttpStatus.OK).build() }
 
 }
