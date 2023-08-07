491907827c70797f5ac07e6decad2da2db7845c1
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-07 14:40:08 +0900

    :sparkles: :: increaseAuthCodeCount 메서드 추가

--- goms-domain/src/main/kotlin/com/goms/v2/domain/email/Authentication.kt
+++ goms-domain/src/main/kotlin/com/goms/v2/domain/email/Authentication.kt
@@ -6,12 +6,17 @@ import com.goms.v2.common.annotation.RootAggregate
 data class Authentication(
     val email: String,
     var attemptCount: Int,
+    var authCodeCount: Int,
     var isAuthentication: Boolean,
     val expiredAt: Int
 ) {
 
     fun certified() {
         this.isAuthentication = true
+    }
+
+    fun increaseAuthCodeCount() {
+        this.authCodeCount += 1
     }
 
     fun increaseAttemptCount() {
