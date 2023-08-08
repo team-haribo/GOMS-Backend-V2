b1745dd7910f719284d73b67bff66abc949e0a05
yoontaebin <gusckd000@gmail.com>
2023-08-08 19:44:37 +0900

    :sparkles: :: 블랙리스트 삭제 api 추가

--- goms-presentation/src/main/kotlin/com/goms/v2/domain/studentCouncil/StudentCouncilController.kt
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/studentCouncil/StudentCouncilController.kt
@@ -1,9 +1,11 @@
 package com.goms.v2.domain.studentCouncil
 
 import com.goms.v2.domain.studentCouncil.usecase.CreateOutingUseCase
+import com.goms.v2.domain.studentCouncil.usecase.DeleteOutingBlacklistUseCase
 import com.goms.v2.domain.studentCouncil.usecase.SaveOutingBlackListUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,7 +16,8 @@ import java.util.UUID
 @RequestMapping("/api/v1/student-council")
 class StudentCouncilController(
     private val createOutingUseCase: CreateOutingUseCase,
-    private val saveOutingBlackListUseCase: SaveOutingBlackListUseCase
+    private val saveOutingBlackListUseCase: SaveOutingBlackListUseCase,
+    private val deleteOutingBlacklistUseCase: DeleteOutingBlacklistUseCase
 ) {
 
     @PostMapping("outing")
@@ -26,5 +29,10 @@ class StudentCouncilController(
     fun saveBlackList(@PathVariable accountIdx: UUID): ResponseEntity<Void> =
         saveOutingBlackListUseCase.execute(accountIdx)
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @DeleteMapping("black-list/{accountIdx}")
+    fun deleteBlackList(@PathVariable accountIdx: UUID): ResponseEntity<Void> =
+        deleteOutingBlacklistUseCase.execute(accountIdx)
+            .let { ResponseEntity.status(HttpStatus.RESET_CONTENT).build() }
 
 }
