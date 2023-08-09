cfece635276daef1a7f839c463bc9b5b562e2b35
yoontaebin <gusckd000@gmail.com>
2023-08-10 02:41:03 +0900

    :sparkles: :: outing jpa entity 추가

---
+++ goms-domain/src/main/kotlin/com/goms/v2/repository/outing/OutingRepository.kt
@@ -0,0 +1,13 @@
+package com.goms.v2.repository.outing
+
+import com.goms.v2.domain.account.Account
+import com.goms.v2.domain.outing.Outing
+import java.util.UUID
+
+interface OutingRepository {
+
+    fun save(outing: Outing)
+    fun deleteByAccountIdx(accountIdx: UUID)
+    fun existsByAccount(account: Account): Boolean
+
+}
---
+++ goms-infrastructure/src/main/kotlin/com/goms/v2/persistence/outing/repository/OutingRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package com.goms.v2.persistence.outing.repository
+
+import com.goms.v2.domain.account.Account
+import com.goms.v2.domain.outing.Outing
+import com.goms.v2.persistence.account.mapper.AccountMapper
+import com.goms.v2.persistence.outing.mapper.OutingMapper
+import com.goms.v2.repository.outing.OutingRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+class OutingRepositoryImpl(
+    private val outingJpaRepository: OutingJpaRepository,
+    private val outingMapper: OutingMapper,
+    private val accountMapper: AccountMapper
+): OutingRepository {
+
+    override fun save(outing: Outing) {
+        val outingEntity = outingMapper.toEntity(outing)
+        outingJpaRepository.save(outingEntity)
+    }
+
+    override fun deleteByAccountIdx(accountIdx: UUID) {
+        outingJpaRepository.deleteByAccountIdx(accountIdx)
+    }
+
+    override fun existsByAccount(account: Account): Boolean {
+        val accountEntity = accountMapper.toEntity(account)
+        return outingJpaRepository.existsByAccount(accountEntity)
+    }
+
+}
