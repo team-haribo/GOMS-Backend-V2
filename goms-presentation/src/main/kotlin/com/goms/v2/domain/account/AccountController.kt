309fe02b5858503b741f240753a852bffe6b037d
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-11 01:01:27 +0900

    :sparkles: :: 프로필 반환 api

---
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/account/AccountController.kt
@@ -0,0 +1,24 @@
+package com.goms.v2.domain.account
+
+import com.goms.v2.domain.account.dto.response.ProfileHttpResponse
+import com.goms.v2.domain.account.mapper.AccountDataMapper
+import com.goms.v2.domain.account.usecase.QueryAccountProfileUseCase
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("api/v1/account")
+class AccountController(
+    private val accountDataMapper: AccountDataMapper,
+    private val queryAccountProfileUseCase: QueryAccountProfileUseCase
+) {
+
+    @GetMapping("profile")
+    fun queryProfile(): ResponseEntity<ProfileHttpResponse> =
+        queryAccountProfileUseCase.execute()
+            .let { ResponseEntity.ok(accountDataMapper.toEntity(it)) }
+
+}
