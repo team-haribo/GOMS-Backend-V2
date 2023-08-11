197781fa697bbbd77dd0a6f82fb48d39d288cbc0
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-11 19:43:04 +0900

    :sparkles: :: 현재 외출중인 학생 반환 테스트코드 추가

---
+++ goms-presentation/src/test/kotlin/com/goms/v2/domain/outing/QueryOutingAccountUseCaseTest.kt
@@ -0,0 +1,36 @@
+package com.goms.v2.domain.outing
+
+import com.goms.v2.common.AnyValueObjectGenerator
+import com.goms.v2.domain.account.Account
+import com.goms.v2.domain.outing.data.dto.OutingAccountDto
+import com.goms.v2.domain.outing.usecase.QueryOutingAccountUseCase
+import com.goms.v2.repository.outing.OutingRepository
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import java.time.LocalTime
+import java.util.*
+
+class QueryOutingAccountUseCaseTest: BehaviorSpec({
+    val outingRepository = mockk<OutingRepository>()
+    val queryOutingAccountUseCase = QueryOutingAccountUseCase(outingRepository)
+
+    Given("외출자가 생길때") {
+        val createdTime = LocalTime.now()
+        val accountIdx = UUID.randomUUID()
+        val account = AnyValueObjectGenerator.anyValueObject<Account>("idx" to accountIdx)
+        val outing = AnyValueObjectGenerator.anyValueObject<Outing>("account" to account, "createdTime" to createdTime)
+        val outingAccountDto = AnyValueObjectGenerator.anyValueObject<OutingAccountDto>("accountIdx" to accountIdx, "createdTime" to createdTime)
+
+        every { outingRepository.queryAllByOrderByCreatedTimeDesc() } returns listOf(outing)
+
+        When("외출자 목록 요청을 하면") {
+            val result = queryOutingAccountUseCase.execute()
+
+            Then("result와 outingAccountDto은 같아야 한다.") {
+                result shouldBe listOf(outingAccountDto)
+            }
+        }
+    }
+})
