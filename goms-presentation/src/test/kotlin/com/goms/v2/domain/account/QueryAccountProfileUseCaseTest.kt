766c51d9732dd29f574a003cb7bbba1165f85809
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-11 12:17:55 +0900

    :sparkles: :: 테스트 케이스 추가

--- goms-presentation/src/test/kotlin/com/goms/v2/domain/account/QueryAccountProfileUseCaseTest.kt
+++ goms-presentation/src/test/kotlin/com/goms/v2/domain/account/QueryAccountProfileUseCaseTest.kt
@@ -4,10 +4,12 @@ import com.goms.v2.common.AnyValueObjectGenerator
 import com.goms.v2.common.util.AccountUtil
 import com.goms.v2.domain.account.data.dto.ProfileDto
 import com.goms.v2.domain.account.usecase.QueryAccountProfileUseCase
+import com.goms.v2.domain.auth.exception.AccountNotFoundException
 import com.goms.v2.repository.account.AccountRepository
 import com.goms.v2.repository.late.LateRepository
 import com.goms.v2.repository.outing.OutingBlackListRepository
 import com.goms.v2.repository.outing.OutingRepository
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -37,6 +39,16 @@ class QueryAccountProfileUseCaseTest: BehaviorSpec({
 
             Then("result와 profileDto는 같아야 한다.") {
                 result shouldBe profileDto
+            }
+        }
+
+        When("없는 계정으로 요청을 하면") {
+            every { accountRepository.findByIdOrNull(account.idx) } returns null
+
+            Then("AccountNotFoundExceptino이 터져야 한다.") {
+                shouldThrow<AccountNotFoundException> {
+                    queryAccountProfileUseCase.execute()
+                }
             }
         }
     }
