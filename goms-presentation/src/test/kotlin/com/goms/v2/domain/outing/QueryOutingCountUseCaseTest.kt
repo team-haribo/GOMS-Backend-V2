a7eeb7c175833eb25820d21a6ecea6cd48c9bf45
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-11 22:28:52 +0900

    :sparkles: :: 외출 학생 카운트 테스트코드

---
+++ goms-presentation/src/test/kotlin/com/goms/v2/domain/outing/QueryOutingCountUseCaseTest.kt
@@ -0,0 +1,27 @@
+package com.goms.v2.domain.outing
+
+import com.goms.v2.domain.outing.usecase.QueryOutingCountUseCase
+import com.goms.v2.repository.outing.OutingRepository
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+
+class QueryOutingCountUseCateTest: BehaviorSpec({
+    val outingRepository = mockk<OutingRepository>()
+    val queryOutingCountUseCase = QueryOutingCountUseCase(outingRepository)
+
+    Given("외출자가 생길때") {
+        val outingCount = 1L
+
+        every { outingRepository.count() } returns outingCount
+
+        When("외출자 카운트 요청을 하면") {
+            val result = queryOutingCountUseCase.execute()
+
+            Then("result와 outingCount는 같아야 한다.") {
+                result shouldBe outingCount
+            }
+        }
+    }
+})
