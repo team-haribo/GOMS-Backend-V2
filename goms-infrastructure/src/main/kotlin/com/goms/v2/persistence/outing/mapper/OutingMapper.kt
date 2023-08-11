402b63537c85d7e0ea7d426791f367989354e5ec
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-11 19:37:27 +0900

    :sparkles: :: toDomain Mapper 추가

--- goms-infrastructure/src/main/kotlin/com/goms/v2/persistence/outing/mapper/OutingMapper.kt
+++ goms-infrastructure/src/main/kotlin/com/goms/v2/persistence/outing/mapper/OutingMapper.kt
@@ -28,4 +28,15 @@ interface OutingMapper {
     )
     fun toEntity(outing: Outing): OutingJpaEntity
 
+    @Mappings(
+        Mapping(target = "account.idx", source = "account.idx"),
+        Mapping(target = "account.email", source = "account.email"),
+        Mapping(target = "account.studentNumber", source = "account.studentNumber"),
+        Mapping(target = "account.name", source = "account.name"),
+        Mapping(target = "account.profileUrl", source = "account.profileUrl"),
+        Mapping(target = "account.authority", source = "account.authority"),
+        Mapping(target = "account.createdTime", source = "account.createdTime")
+    )
+    fun toDomain(outingJpaEntity: OutingJpaEntity): Outing
+
 }
