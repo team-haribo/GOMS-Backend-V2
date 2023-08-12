c661aa8700c4ecbc4894fe4ac2b3351d1f229e73
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-12 09:03:29 +0900

    :sparkles: :: 외출 학생 검색 api

--- goms-presentation/src/main/kotlin/com/goms/v2/domain/outing/OutingController.kt
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/outing/OutingController.kt
@@ -6,6 +6,7 @@ import com.goms.v2.domain.outing.mapper.OutingDataMapper
 import com.goms.v2.domain.outing.usecase.OutingUseCase
 import com.goms.v2.domain.outing.usecase.QueryOutingAccountUseCase
 import com.goms.v2.domain.outing.usecase.QueryOutingCountUseCase
+import com.goms.v2.domain.outing.usecase.SearchOutingUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -17,7 +18,8 @@ class OutingController(
     private val outingDataMapper: OutingDataMapper,
     private val outingUseCase: OutingUseCase,
     private val queryOutingAccountUseCase: QueryOutingAccountUseCase,
-    private val queryOutingCountUseCase: QueryOutingCountUseCase
+    private val queryOutingCountUseCase: QueryOutingCountUseCase,
+    private val searchOutingUseCase: SearchOutingUseCase
 ) {
 
     @PostMapping("{outingUUID}")
@@ -36,5 +38,11 @@ class OutingController(
         queryOutingCountUseCase.execute()
             .let { outingDataMapper.toResponse(it) }
             .let { ResponseEntity.ok(it) }
+
+    @GetMapping("search")
+    fun searchOutingAccount(name: String?): ResponseEntity<List<OutingAccountHttpResponse>> =
+        searchOutingUseCase.execute(name)
+            .map { outingDataMapper.toResponse(it) }
+            .let { ResponseEntity.ok(it)}
 
 }
