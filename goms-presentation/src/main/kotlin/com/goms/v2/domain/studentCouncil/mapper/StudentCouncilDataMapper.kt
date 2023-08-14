2755f5aeea333d3369796525638b5b7ebd25f815
yoontaebin <gusckd000@gmail.com>
2023-08-14 18:43:09 +0900

    :sparkles: :: StudentCouncilDataMapper 추가

---
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/studentCouncil/mapper/StudentCouncilDataMapper.kt
@@ -0,0 +1,21 @@
+package com.goms.v2.domain.studentCouncil.mapper
+
+import com.goms.v2.domain.studentCouncil.data.dto.AllAccountDto
+import com.goms.v2.domain.studentCouncil.dto.response.AllAccountHttpResponse
+import org.mapstruct.*
+
+@Mapper(
+    componentModel = MappingConstants.ComponentModel.SPRING,
+    injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+    unmappedTargetPolicy = ReportingPolicy.WARN
+)
+interface StudentCouncilDataMapper {
+
+    @Mappings(
+        Mapping(target = "studentNum.grade", source = "studentNum.grade"),
+        Mapping(target = "studentNum.classNum", source = "studentNum.classNum"),
+        Mapping(target = "studentNum.number", source = "studentNum.number")
+    )
+    fun toResponse(allAccountDto: AllAccountDto): AllAccountHttpResponse
+
+}
