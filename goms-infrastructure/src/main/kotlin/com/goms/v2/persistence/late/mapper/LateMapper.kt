a51126ac1b36c3b7c67da6a4e276d893d4ecbc24
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-14 19:56:02 +0900

    :sparkles: :: LateMapper 추가

---
+++ goms-infrastructure/src/main/kotlin/com/goms/v2/persistence/late/mapper/LateMapper.kt
@@ -0,0 +1,26 @@
+package com.goms.v2.persistence.late.mapper
+
+import com.goms.v2.domain.late.Late
+import com.goms.v2.persistence.late.entity.LateJpaEntity
+import org.mapstruct.*
+import org.springframework.stereotype.Component
+
+@Mapper(
+    componentModel = MappingConstants.ComponentModel.SPRING,
+    injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+    unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+@Component
+interface LateMapper {
+    @Mappings(
+        Mapping(target = "account.idx", source = "account.idx"),
+        Mapping(target = "account.email", source = "account.email"),
+        Mapping(target = "account.studentNumber", source = "account.studentNumber"),
+        Mapping(target = "account.name", source = "account.name"),
+        Mapping(target = "account.profileUrl", source = "account.profileUrl"),
+        Mapping(target = "account.authority", source = "account.authority"),
+        Mapping(target = "account.createdTime", source = "account.createdTime")
+    )
+    fun toEntity(late: Late): LateJpaEntity
+
+}
