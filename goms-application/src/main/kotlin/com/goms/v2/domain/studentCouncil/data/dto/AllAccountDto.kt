07948c36a407756134affc4b03096c85d0d7b0a2
yoontaebin <gusckd000@gmail.com>
2023-08-14 18:38:58 +0900

    :sparkles: :: AllAccountDto 추가

---
+++ goms-application/src/main/kotlin/com/goms/v2/domain/studentCouncil/data/dto/AllAccountDto.kt
@@ -0,0 +1,14 @@
+package com.goms.v2.domain.studentCouncil.data.dto
+
+import com.goms.v2.domain.account.Authority
+import com.goms.v2.domain.account.data.dto.StudentNumberDto
+import java.util.UUID
+
+data class AllAccountDto(
+    val accountIdx: UUID,
+    val name: String,
+    val studentNum: StudentNumberDto,
+    val profileUrl: String?,
+    val authority: Authority,
+    val isBlackList: Boolean
+)
