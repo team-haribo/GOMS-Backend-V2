6665b83a7598f69cb2a1850f6a336b4a7e3190e0
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-14 19:55:55 +0900

    :sparkles: :: saveAll 메서드 추가

--- goms-domain/src/main/kotlin/com/goms/v2/repository/late/LateRepository.kt
+++ goms-domain/src/main/kotlin/com/goms/v2/repository/late/LateRepository.kt
@@ -1,9 +1,11 @@
 package com.goms.v2.repository.late
 
+import com.goms.v2.domain.late.Late
 import java.util.*
 
 interface LateRepository {
 
     fun countByAccountIdx(accountIdx: UUID): Long
+    fun saveAll(lateList: List<Late>)
 
 }
