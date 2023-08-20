7495e55f0d88521e848ca1b67e3d6a5c01b53d07
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-21 03:04:11 +0900

    :fire: :: 로그인, 토큰 재발급 controller 테스트코드 작성

--- goms-presentation/src/test/kotlin/com/goms/v2/domain/auth/AuthControllerTest.kt
+++ goms-presentation/src/test/kotlin/com/goms/v2/domain/auth/AuthControllerTest.kt
@@ -1,95 +1,126 @@
 package com.goms.v2.domain.auth
 
-import com.goms.v2.common.AnyValueObjectGenerator
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.goms.v2.domain.account.Authority
+import com.goms.v2.domain.auth.data.dto.SignInDto
+import com.goms.v2.domain.auth.data.dto.TokenDto
+import com.goms.v2.domain.auth.dto.request.SignInHttpRequest
 import com.goms.v2.domain.auth.dto.response.TokenHttpResponse
 import com.goms.v2.domain.auth.mapper.AuthDataMapper
 import com.goms.v2.domain.auth.usecase.ReissueTokenUseCase
 import com.goms.v2.domain.auth.usecase.SignInUseCase
-import com.nhaarman.mockitokotlin2.mock
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.every
+import io.mockk.mockk
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-@SpringBootTest(classes = [AuthControllerTest::class])
-@AutoConfigureMockMvc
-class AuthControllerTest {
+class AuthControllerTest: DescribeSpec({
+    lateinit var mockMvc: MockMvc
+    val authDataMapper = mockk<AuthDataMapper>()
+    val signInUseCase = mockk<SignInUseCase>()
+    val reissueTokenUseCase = mockk<ReissueTokenUseCase>()
+    val authController = AuthController(
+        authDataMapper,
+        signInUseCase,
+        reissueTokenUseCase
+    )
 
-    private lateinit var mockMvc: MockMvc
-    val authDataMapper = mock<AuthDataMapper>()
-    val signInUseCase = mock<SignInUseCase>()
-    val reissueTokenUseCase = mock<ReissueTokenUseCase>()
-    val authController = AuthController(authDataMapper, signInUseCase, reissueTokenUseCase)
-
-    @BeforeEach
-    fun setup() {
+    beforeTest {
         mockMvc = MockMvcBuilders.standaloneSetup(authController).build()
     }
 
-    @Test
-    fun `로그인 성공`() {
-        val tokenHttpResponse = AnyValueObjectGenerator.anyValueObject<TokenHttpResponse>(
-            "accessToken" to "accessToken",
-            "refreshToken" to "refreshToken",
-            "accessTokenExp" to LocalDateTime.now(),
-            "refreshTokenExp" to LocalDateTime.now(),
-            "authority" to Authority.ROLE_STUDENT
-        )
+    describe("POST : /api/v2/auth/signin") {
+        val url = "/api/v2/auth/signin"
 
-        val signInJson = """
-            {
-                "code": "test GAuthCode"
+        context("유효한 요청이 전달 되면") {
+            val signInHttpRequest = SignInHttpRequest("code")
+            val tokenDto = TokenDto(
+                accessToken = "accessToken",
+                refreshToken = "refreshToken",
+                accessTokenExp = 0,
+                refreshTokenExp = 0,
+                authority = Authority.ROLE_STUDENT
+            )
+            val tokenHttpResponse = TokenHttpResponse(
+                accessToken = "accessToken",
+                refreshToken = "refreshToken",
+                accessTokenExp = LocalDateTime.parse("2023-08-20T23:23:14"),
+                refreshTokenExp = LocalDateTime.parse("2023-08-20T23:23:14"),
+                authority =  Authority.ROLE_STUDENT
+            )
+            val signInDto = SignInDto(code = "code")
+
+            every { authDataMapper.toDto(signInHttpRequest) } returns signInDto
+            every { signInUseCase.execute(signInDto) } returns tokenDto
+            every { authDataMapper.toResponse(tokenDto) } returns tokenHttpResponse
+
+            val jsonRequestBody = jacksonObjectMapper().writeValueAsString(signInHttpRequest)
+
+            it("TokenHttpResponse를 반환한다.") {
+                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+
+                mockMvc.perform(
+                    post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody)
+                )
+                    .andExpect(status().`is`(200))
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(jsonPath("$.accessToken").value(tokenHttpResponse.accessToken))
+                    .andExpect(jsonPath("$.refreshToken").value(tokenHttpResponse.refreshToken))
+                    .andExpect(jsonPath("$.accessTokenExp").value(formatter.format(tokenHttpResponse.accessTokenExp)))
+                    .andExpect(jsonPath("$.refreshTokenExp").value(formatter.format(tokenHttpResponse.refreshTokenExp)))
+                    .andExpect(jsonPath("$.authority").value(tokenHttpResponse.authority.toString()))
+                    .andDo(MockMvcResultHandlers.print())
             }
-        """.trimIndent()
-
-        mockMvc.perform(
-            post("/api/v2/auth/signin")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(signInJson)
-        )
-            .andExpect{ status().isOk }
-            .andExpect{content().contentType(MediaType.APPLICATION_JSON_VALUE)}
-            .andExpect{jsonPath("$.accessToken").value(tokenHttpResponse.accessToken)}
-            .andExpect{jsonPath("$.refreshToken").value(tokenHttpResponse.refreshToken)}
-            .andExpect{jsonPath("$.accessTokenExp").value(tokenHttpResponse.accessTokenExp)}
-            .andExpect{jsonPath("$.refreshTokenExp").value(tokenHttpResponse.refreshTokenExp)}
-            .andExpect{jsonPath("$.authority").value(tokenHttpResponse.authority)}
-            .andDo{MockMvcResultHandlers.print()}
-
+        }
     }
+    describe("PATCH : /api/v2/auth") {
+        val url = "/api/v2/auth"
 
-    @Test
-    fun `토큰 재발급 성공`() {
-        val refreshToken = "testRefreshToken"
-        val tokenHttpResponse = AnyValueObjectGenerator.anyValueObject<TokenHttpResponse>(
-            "accessToken" to "accessToken",
-            "refreshToken" to "refreshToken",
-            "accessTokenExp" to LocalDateTime.now(),
-            "refreshTokenExp" to LocalDateTime.now(),
-            "authority" to Authority.ROLE_STUDENT
-        )
+        context("유효한 요청이 전달 되면") {
+            val tokenDto = TokenDto(
+                accessToken = "accessToken",
+                refreshToken = "refreshToken",
+                accessTokenExp = 0,
+                refreshTokenExp = 0,
+                authority = Authority.ROLE_STUDENT
+            )
+            val tokenHttpResponse = TokenHttpResponse(
+                accessToken = "accessToken",
+                refreshToken = "refreshToken",
+                accessTokenExp = LocalDateTime.parse("2023-08-20T23:23:14"),
+                refreshTokenExp = LocalDateTime.parse("2023-08-20T23:23:14"),
+                authority = Authority.ROLE_STUDENT
+            )
 
-        mockMvc.perform(
-            patch("/api/v2/auth")
-                .header("refreshToken", refreshToken)
-        )
-            .andExpect{ status().isOk }
-            .andExpect{content().contentType(MediaType.APPLICATION_JSON_VALUE)}
-            .andExpect{jsonPath("$.accessToken").value(tokenHttpResponse.accessToken)}
-            .andExpect{jsonPath("$.refreshToken").value(tokenHttpResponse.refreshToken)}
-            .andExpect{jsonPath("$.accessTokenExp").value(tokenHttpResponse.accessTokenExp)}
-            .andExpect{jsonPath("$.refreshTokenExp").value(tokenHttpResponse.refreshTokenExp)}
-            .andExpect{jsonPath("$.authority").value(tokenHttpResponse.authority)}
-            .andDo{MockMvcResultHandlers.print()}
+            every { reissueTokenUseCase.execute("refreshToken") } returns tokenDto
+            every { authDataMapper.toResponse(tokenDto) } returns tokenHttpResponse
+
+            it("TokenHttpResponse를 반환한다.") {
+                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+
+                mockMvc.perform(
+                    patch(url)
+                        .header("refreshToken", "refreshToken")
+                )
+                    .andExpect(status().`is`(200))
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(jsonPath("$.accessToken").value(tokenHttpResponse.accessToken))
+                    .andExpect(jsonPath("$.refreshToken").value(tokenHttpResponse.refreshToken))
+                    .andExpect(jsonPath("$.accessTokenExp").value(formatter.format(tokenHttpResponse.accessTokenExp)))
+                    .andExpect(jsonPath("$.refreshTokenExp").value(formatter.format(tokenHttpResponse.refreshTokenExp)))
+                    .andExpect(jsonPath("$.authority").value(tokenHttpResponse.authority.toString()))
+                    .andDo(MockMvcResultHandlers.print())
+            }
+        }
     }
-
-}
+})
