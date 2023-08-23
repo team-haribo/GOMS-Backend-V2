9fdb5d0ae7ec67712f9b31829b73bffd6a22fc27
yoontaebin <gusckd000@gmail.com>
2023-08-24 00:11:22 +0900

    :sparkles: :: SearchAccountUseCase 추가

---
+++ goms-application/src/main/kotlin/com/goms/v2/domain/studentCouncil/usecase/SearchAccountUseCase.kt
@@ -0,0 +1,37 @@
+package com.goms.v2.domain.studentCouncil.usecase
+
+import com.goms.v2.common.annotation.UseCaseWithTransaction
+import com.goms.v2.domain.account.Authority
+import com.goms.v2.domain.account.data.dto.StudentNumberDto
+import com.goms.v2.domain.studentCouncil.data.dto.AccountDto
+import com.goms.v2.repository.account.AccountRepository
+import com.goms.v2.repository.outing.OutingBlackListRepository
+import kotlin.streams.asSequence
+
+@UseCaseWithTransaction
+class SearchAccountUseCase(
+    private val accountRepository: AccountRepository,
+    private val outingBlackListRepository: OutingBlackListRepository
+) {
+
+    fun execute(grade: Int?, classNum: Int?, name: String?, authority: Authority?, isBlackList: Boolean?): List<AccountDto> {
+        val outingBlackLIstIdx = outingBlackListRepository.findAll().map { it.accountIdx }
+
+        return accountRepository.findAccountByStudentInfo(grade, classNum, name, authority).stream().asSequence()
+            .filter {
+                if (isBlackList != null && isBlackList) outingBlackLIstIdx.contains(it.idx)
+                else if (isBlackList != null) outingBlackLIstIdx.contains(it.idx).not()
+                else true
+            }.map {
+                AccountDto(
+                    accountIdx = it.idx,
+                    name = it.name,
+                    studentNum = StudentNumberDto(it.studentNumber.grade, it.studentNumber.classNum, it.studentNumber.number),
+                    profileUrl = it.profileUrl,
+                    authority = it.authority,
+                    isBlackList = outingBlackLIstIdx.contains(it.idx)
+                )
+            }.toList()
+    }
+
+}
