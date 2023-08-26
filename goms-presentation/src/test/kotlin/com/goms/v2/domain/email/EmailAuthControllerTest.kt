89451d1cd14704db355581790358a6d437238917
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-26 14:45:03 +0900

    :sparkles: :: 이메일 인증번호 인증 controller 테스트 코드

--- goms-presentation/src/test/kotlin/com/goms/v2/domain/email/EmailAuthControllerTest.kt
+++ goms-presentation/src/test/kotlin/com/goms/v2/domain/email/EmailAuthControllerTest.kt
@@ -1,6 +1,9 @@
 package com.goms.v2.domain.email
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.goms.v2.domain.account.Authority
+import com.goms.v2.domain.auth.data.dto.TokenDto
+import com.goms.v2.domain.auth.dto.response.TokenHttpResponse
 import com.goms.v2.domain.auth.mapper.AuthDataMapper
 import com.goms.v2.domain.email.data.dto.EmailDto
 import com.goms.v2.domain.email.dto.request.SendEmailHttpRequest
@@ -13,9 +16,15 @@ import io.mockk.mockk
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.util.LinkedMultiValueMap
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
 
 class EmailAuthControllerTest: DescribeSpec({
     lateinit var mockMvc: MockMvc
@@ -55,6 +64,54 @@ class EmailAuthControllerTest: DescribeSpec({
                         .content(jsonRequestBody)
                 )
                     .andExpect(status().`is`(204))
+                    .andDo(MockMvcResultHandlers.print())
+            }
+        }
+    }
+
+    describe("api/v2/email/verify 로 get 요청을 했을때") {
+        val url = "/api/v2/email/verify"
+
+        context("유효한 요청이 전달 되면") {
+            val email = "test@test.com"
+            val authCode = "1234"
+            val tokenDto = TokenDto(
+                accessToken = "accessToken",
+                refreshToken = "refreshToken",
+                accessTokenExp = 0,
+                refreshTokenExp = 0,
+                authority = Authority.ROLE_STUDENT
+            )
+            val tokenHttpResponse = TokenHttpResponse(
+                accessToken = "accessToken",
+                refreshToken = "refreshToken",
+                accessTokenExp = LocalDateTime.parse("2023-08-20T23:23:14"),
+                refreshTokenExp = LocalDateTime.parse("2023-08-20T23:23:14"),
+                authority = Authority.ROLE_STUDENT
+            )
+
+            every { verifyAuthCodeUseCase.execute(email, authCode) } returns tokenDto
+            every { authDataMapper.toResponse(tokenDto) } returns tokenHttpResponse
+
+            it("TokenHttpResponse를 반환한다.") {
+                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+                val requestParam = LinkedMultiValueMap<String, String>()
+
+                requestParam.add("email", email)
+                requestParam.add("authCode", authCode)
+
+                mockMvc.perform(
+                    get(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .params(requestParam)
+                )
+                    .andExpect(status().`is`(200))
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(jsonPath("$.accessToken").value(tokenHttpResponse.accessToken))
+                    .andExpect(jsonPath("$.refreshToken").value(tokenHttpResponse.refreshToken))
+                    .andExpect(jsonPath("$.accessTokenExp").value(formatter.format(tokenHttpResponse.accessTokenExp)))
+                    .andExpect(jsonPath("$.refreshTokenExp").value(formatter.format(tokenHttpResponse.refreshTokenExp)))
+                    .andExpect(jsonPath("$.authority").value(tokenHttpResponse.authority.toString()))
                     .andDo(MockMvcResultHandlers.print())
             }
         }
