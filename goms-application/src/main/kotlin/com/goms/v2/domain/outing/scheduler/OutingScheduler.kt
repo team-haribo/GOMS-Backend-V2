a6958500404363988ec46b011176c646d7b5f019
KimGyeongsuuu <sk62628504@gmail.com>
2023-08-30 20:06:23 +0900

    :recycle: :: 지각자 저장 시간 26분으로 변경

--- goms-application/src/main/kotlin/com/goms/v2/domain/outing/scheduler/OutingScheduler.kt
+++ goms-application/src/main/kotlin/com/goms/v2/domain/outing/scheduler/OutingScheduler.kt
@@ -11,7 +11,7 @@ class OutingScheduler(
     private val deleteOutingStudentsUseCase: DeleteOutingStudentsUseCase,
 ) {
 
-    @Scheduled(cron = "0 25 19 ? * 3") // 매주 수요일 7시 25분에 지각자를 저장한다.
+    @Scheduled(cron = "0 26 19 ? * 3") // 매주 수요일 7시 26분에 지각자를 저장한다.
     fun checkRateStudent() = saveLateAccountUseCase.execute()
 
     @Scheduled(cron = "0 50 19 ? * 3") // 매주 수요일 7시 50분에 외출자를 삭제한다.
