1291be68d154882d6e523ad506c6080471d35ad2
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-09-01 12:20:34 +0900

    :sparkles: :: deviceToken 도메인 모델 영속화 하기 위한 repository 추가

---
+++ goms-infrastructure/src/main/kotlin/com/goms/v2/persistence/notification/entity/DeviceTokenEntity.kt
@@ -0,0 +1,12 @@
+package com.goms.v2.persistence.notification.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import java.util.*
+
+@RedisHash("device_token")
+data class DeviceTokenEntity(
+    @Id
+    val accountIdx: UUID,
+    val token: String
+)
---
+++ goms-infrastructure/src/main/kotlin/com/goms/v2/persistence/notification/repository/DeviceTokenRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package com.goms.v2.persistence.notification.repository
+
+import com.goms.v2.domain.notification.DeviceToken
+import com.goms.v2.persistence.notification.mapper.DeviceTokenMapper
+import com.goms.v2.repository.notification.DeviceTokenRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import java.util.UUID
+
+@Component
+class DeviceTokenRepositoryImpl(
+    private val deviceTokenJpaRepository: DeviceTokenJpaRepository,
+    private val deviceTokenMapper: DeviceTokenMapper
+): DeviceTokenRepository {
+
+    override fun save(deviceToken: DeviceToken) {
+        val deviceTokenEntity = deviceTokenMapper.toEntity(deviceToken)
+        deviceTokenJpaRepository.save(deviceTokenEntity)
+    }
+
+    override fun findAll(): List<DeviceToken> {
+        val deviceTokenEntityList = deviceTokenJpaRepository.findAll().filterNotNull()
+        println(deviceTokenJpaRepository.findAll())
+        return deviceTokenEntityList.map { deviceTokenMapper.toDomain(it) }
+    }
+
+    override fun findByIdxOrNull(accountIdx: UUID): DeviceToken? {
+        val deviceTokenEntity = deviceTokenJpaRepository.findByIdOrNull(accountIdx)
+        return deviceTokenEntity?.let { deviceTokenMapper.toDomain(it) }
+    }
+
+}
