3148b13d47afc53c38a1c94b286d1760c1baeeff
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-09-01 12:26:54 +0900

    :sparkles: :: 디바이스 토큰 설정, notification 설정 api 추가

---
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/notification/NotificationController.kt
@@ -0,0 +1,35 @@
+package com.goms.v2.domain.notification
+
+import com.goms.v2.domain.notification.usecase.SendNotificationUseCase
+import com.goms.v2.domain.notification.usecase.SetDeviceTokenUseCase
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v2/notification")
+class NotificationController(
+    private val setDeviceTokenUseCase: SetDeviceTokenUseCase,
+    private val sendNotificationUseCase: SendNotificationUseCase
+) {
+
+    @PostMapping("token/{deviceToken}")
+    fun setDeviceToken(@PathVariable deviceToken: String): ResponseEntity<Void> =
+        setDeviceTokenUseCase.execute(deviceToken)
+            .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
+
+    @GetMapping("outing/before")
+    fun beforeOutingTestApi(): ResponseEntity<Map<String, String>> =
+        sendNotificationUseCase.execute(NotificationType.BEFORE_OUTING)
+            .run { ResponseEntity.ok(mapOf("message" to "success")) }
+
+    @GetMapping("outing/after")
+    fun afterOutingTestApi(): ResponseEntity<Map<String, String>> =
+        sendNotificationUseCase.execute(NotificationType.AFTER_OUTING)
+            .run { ResponseEntity.ok(mapOf("message" to "success")) }
+
+}
