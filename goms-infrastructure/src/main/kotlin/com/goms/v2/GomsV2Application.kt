3ceaa6423415f2751f5f25f04e2cfb9735c56f28
KimGyeongsuuu <sk62628504@gmail.com>
2023-09-02 20:46:13 +0900

    :recycle: :: timeZone 설정하기

--- goms-infrastructure/src/main/kotlin/com/goms/v2/GomsV2Application.kt
+++ goms-infrastructure/src/main/kotlin/com/goms/v2/GomsV2Application.kt
@@ -1,11 +1,26 @@
 package com.goms.v2
 
+import mu.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableAsync
+import java.time.LocalDateTime
+import java.util.*
+import javax.annotation.PostConstruct
+
+private val log = KotlinLogging.logger {}
 
 @SpringBootApplication
-class GomsV2Application
+class GomsV2Application {
+
+    @PostConstruct
+    fun applicationTimeZoneSetter() {
+        val timeZone = TimeZone.getTimeZone("Asia/Seoul")
+        TimeZone.setDefault(timeZone)
+
+        log.info { "Goms Application TimeZone was set: ${LocalDateTime.now()}" }
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<GomsV2Application>(*args)
