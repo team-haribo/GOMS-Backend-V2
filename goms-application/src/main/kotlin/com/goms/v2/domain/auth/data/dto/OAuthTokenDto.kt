3fa8dad3036998e9fe5a3cfad43dbf5866f5506e
KimGyeongsuuu <sk62628504@gmail.com>
2024-01-18 00:46:51 +0900

    :fire: :: gauth 로그인 코드 삭제

--- goms-application/src/main/kotlin/com/goms/v2/domain/auth/data/dto/OAuthTokenDto.kt
+++
@@ -1,6 +0,0 @@
-package com.goms.v2.domain.auth.data.dto
-
-data class OAuthTokenDto(
-    val accessToken: String,
-    val refreshToken: String
-)
--- goms-application/src/main/kotlin/com/goms/v2/domain/auth/exception/GAuthSecretMismatchException.kt
+++
@@ -1,6 +0,0 @@
-package com.goms.v2.domain.auth.exception
-
-import com.goms.v2.common.exception.ErrorCode
-import com.goms.v2.common.exception.GomsException
-
-class GAuthSecretMismatchException: GomsException(ErrorCode.GAUTH_SECRET_MISMATCH)
