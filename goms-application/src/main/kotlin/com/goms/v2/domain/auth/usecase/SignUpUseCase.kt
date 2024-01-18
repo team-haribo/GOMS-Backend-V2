8862f1b6856c71f3cd352c4abf3dee20102d5305
KimGyeongsuuu <sk62628504@gmail.com>
2024-01-18 16:35:27 +0900

    :sparkles: :: 이메일 받아서 기수 저장하기

--- goms-application/src/main/kotlin/com/goms/v2/domain/auth/usecase/SignUpUseCase.kt
+++ goms-application/src/main/kotlin/com/goms/v2/domain/auth/usecase/SignUpUseCase.kt
@@ -4,7 +4,7 @@ import com.goms.v2.common.annotation.UseCaseWithTransaction
 import com.goms.v2.domain.account.Account
 import com.goms.v2.domain.account.constant.Authority
 import com.goms.v2.domain.auth.data.dto.SignUpDto
-import com.goms.v2.domain.auth.exception.AlreadyExistPhoneNumberException
+import com.goms.v2.domain.auth.exception.AlreadyExistEmailException
 import com.goms.v2.domain.auth.spi.PasswordEncoderPort
 import com.goms.v2.repository.account.AccountRepository
 import java.time.LocalDateTime
@@ -17,22 +17,31 @@ class SignUpUseCase(
 ) {
 
     fun execute(signUpDto: SignUpDto) {
-        if (accountRepository.existsByPhoneNumber(signUpDto.phoneNumber))
-            throw AlreadyExistPhoneNumberException()
+        if (accountRepository.existsByEmail(signUpDto.email))
+            throw AlreadyExistEmailException()
 
         val account = Account(
             idx = UUID.randomUUID(),
-            phoneNumber = signUpDto.phoneNumber,
+            email = signUpDto.email,
             password = passwordEncoderPort.passwordEncode(signUpDto.password),
-            grade = signUpDto.grade,
+            grade = extractGrade(signUpDto.email)!!,
             gender = signUpDto.gender,
+            major = signUpDto.major,
             name = signUpDto.name,
             profileUrl = null,
             authority = Authority.ROLE_STUDENT,
             createdTime = LocalDateTime.now()
         )
-
         accountRepository.save(account)
 
     }
+
+    private fun extractGrade(email: String): Int? =
+        when(email[2]) {
+            '2' -> 6
+            '3' -> 7
+            '4' -> 8
+            else -> null
+        }
+
 }
