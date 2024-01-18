f51dac7ccdc2d144e3c931f7470c535222f78b0f
KimGyeongsuuu <sk62628504@gmail.com>
2024-01-18 20:56:25 +0900

    :sparkles: :: to dto mapper

--- goms-presentation/src/main/kotlin/com/goms/v2/domain/auth/mapper/AuthDataMapper.kt
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/auth/mapper/AuthDataMapper.kt
@@ -1,7 +1,9 @@
 package com.goms.v2.domain.auth.mapper
 
+import com.goms.v2.domain.auth.data.dto.SignInDto
 import com.goms.v2.domain.auth.data.dto.SignUpDto
 import com.goms.v2.domain.auth.data.dto.TokenDto
+import com.goms.v2.domain.auth.dto.request.SignInHttpRequest
 import com.goms.v2.domain.auth.dto.request.SignUpHttpRequest
 import com.goms.v2.domain.auth.dto.response.TokenHttpResponse
 import org.springframework.stereotype.Component
@@ -26,6 +28,12 @@ class AuthDataMapper {
             name = signUpHttpRequest.name,
             major = signUpHttpRequest.major,
             gender = signUpHttpRequest.gender
+        )
+
+    fun toDto(signInHttpRequest: SignInHttpRequest) =
+        SignInDto(
+            email = signInHttpRequest.email,
+            password = signInHttpRequest.password
         )
 
 }
