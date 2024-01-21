afcd6ab8681576682ebdf25742e75f385e70a257
yoontaebin <gusckd000@gmail.com>
2024-01-21 16:31:23 +0900

    :sparkles: :: 이메일 인증 테스트 코드 추가

--- goms-presentation/src/test/kotlin/com/goms/v2/domain/auth/AuthControllerTest.kt
+++ goms-presentation/src/test/kotlin/com/goms/v2/domain/auth/AuthControllerTest.kt
@@ -4,40 +4,47 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.goms.v2.domain.account.constant.Authority
 import com.goms.v2.domain.account.constant.Gender
 import com.goms.v2.domain.account.constant.Major
+import com.goms.v2.domain.auth.data.dto.SendAuthCodeDto
 import com.goms.v2.domain.auth.data.dto.SignInDto
 import com.goms.v2.domain.auth.data.dto.SignUpDto
 import com.goms.v2.domain.auth.data.dto.TokenDto
+import com.goms.v2.domain.auth.dto.request.SendAuthCodeHttpRequest
 import com.goms.v2.domain.auth.dto.request.SignInHttpRequest
 import com.goms.v2.domain.auth.dto.request.SignUpHttpRequest
 import com.goms.v2.domain.auth.dto.response.TokenHttpResponse
+import com.goms.v2.domain.auth.mapper.AuthCodeDataMapper
 import com.goms.v2.domain.auth.mapper.AuthDataMapper
-import com.goms.v2.domain.auth.usecase.ReissueTokenUseCase
-import com.goms.v2.domain.auth.usecase.SignInUseCase
-import com.goms.v2.domain.auth.usecase.SignUpUseCase
+import com.goms.v2.domain.auth.usecase.*
 import io.kotest.core.spec.style.DescribeSpec
 import io.mockk.every
 import io.mockk.mockk
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.util.LinkedMultiValueMap
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class AuthControllerTest: DescribeSpec({
     lateinit var mockMvc: MockMvc
     val authDataMapper = mockk<AuthDataMapper>()
+    val authCodeDataMapper = mockk<AuthCodeDataMapper>()
     val signUpUseCase = mockk<SignUpUseCase>()
     val signInUseCase = mockk<SignInUseCase>()
     val reissueTokenUseCase = mockk<ReissueTokenUseCase>()
+    val sendAuthCodeUseCase = mockk<SendAuthCodeUseCase>()
+    val verifyAuthCodeUseCase = mockk<VerifyAuthCodeUseCase>()
     val authController = AuthController(
         authDataMapper,
+        authCodeDataMapper,
         signUpUseCase,
         signInUseCase,
         reissueTokenUseCase,
+        sendAuthCodeUseCase,
+        verifyAuthCodeUseCase
     )
 
     beforeTest {
@@ -170,6 +177,59 @@ class AuthControllerTest: DescribeSpec({
                     .andExpect(jsonPath("$.refreshTokenExp").value(formatter.format(tokenHttpResponse.refreshTokenExp)))
                     .andExpect(jsonPath("$.authority").value(tokenHttpResponse.authority.toString()))
                     .andDo(MockMvcResultHandlers.print())
+            }
+        }
+    }
+
+    describe("/api/v2/auth/email/send 으로 post 요청을 했을때") {
+        val url = "/api/v2/auth/email/send"
+
+        context("유효한 요청이 전달 되면") {
+            val sendAuthCodeHttpRequest = SendAuthCodeHttpRequest(
+                email = "s22039@gsm.hs.kr"
+            )
+            val sendAuthCodeDto = SendAuthCodeDto(
+                email = "s22039@gsm.hs.kr"
+            )
+
+            every { authCodeDataMapper.toDto(sendAuthCodeHttpRequest) } returns sendAuthCodeDto
+            every { sendAuthCodeUseCase.execute(sendAuthCodeDto) } returns Unit
+
+            val jsonRequestBody = jacksonObjectMapper().writeValueAsString(sendAuthCodeHttpRequest)
+
+            it("204 상태코드를 반환한다.") {
+                mockMvc.perform(
+                    post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody)
+                )
+                    .andExpect(status().`is`(204))
+            }
+        }
+    }
+
+    describe("/api/v2/auth/email/verify 으로 get 요청을 했을때") {
+        val url = "/api/v2/auth/email/verify"
+
+        context("유효한 요청이 전달 되면") {
+            val email = "s22039@gsm.hs.kr"
+            val authCode = "1234"
+
+            every { verifyAuthCodeUseCase.execute(email, authCode) } returns Unit
+
+            it("204 상태코드를 반환한다.") {
+                val requestParam = LinkedMultiValueMap<String, String>()
+
+                requestParam.add("email", email)
+                requestParam.add("authCode", authCode)
+
+                mockMvc.perform(
+                    get(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .params(requestParam)
+                )
+                    .andExpect(status().`is`(204))
+
             }
         }
     }
