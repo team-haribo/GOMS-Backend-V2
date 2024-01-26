eaca3234153686db711b056948e5e26e3ecc53f0
KimGyeongsuuu <sk62628504@gmail.com>
2024-01-26 22:20:28 +0900

    :sparkles: :: 비밀번호 변경 api

--- goms-presentation/src/main/kotlin/com/goms/v2/domain/account/AccountController.kt
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/account/AccountController.kt
@@ -1,10 +1,15 @@
 package com.goms.v2.domain.account
 
+import com.goms.v2.domain.account.dto.request.UpdatePasswordRequest
 import com.goms.v2.domain.account.dto.response.ProfileHttpResponse
 import com.goms.v2.domain.account.mapper.AccountDataMapper
 import com.goms.v2.domain.account.usecase.QueryAccountProfileUseCase
+import com.goms.v2.domain.account.usecase.UpdatePasswordUseCase
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,12 +18,18 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("api/v2/account")
 class AccountController(
     private val accountDataMapper: AccountDataMapper,
-    private val queryAccountProfileUseCase: QueryAccountProfileUseCase
+    private val queryAccountProfileUseCase: QueryAccountProfileUseCase,
+    private val updatePasswordUseCase: UpdatePasswordUseCase
 ) {
 
     @GetMapping("profile")
     fun queryProfile(): ResponseEntity<ProfileHttpResponse> =
         queryAccountProfileUseCase.execute()
             .let { ResponseEntity.ok(accountDataMapper.toResponse(it)) }
+
+    @PatchMapping("new-password")
+    fun updatePassword(@RequestBody updatePasswordRequest: UpdatePasswordRequest): ResponseEntity<Void> =
+        updatePasswordUseCase.execute(accountDataMapper.toDomain(updatePasswordRequest))
+            .let { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
 
 }
