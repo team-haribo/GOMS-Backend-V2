85e9383aec1020c5cb8198075f4c1699f84b104f
KimGyeongsuuu <sk62628504@gmail.com>
2024-01-26 22:20:40 +0900

    :white_check_mark: :: 비밀번호 변경 api 테스트 코드

--- goms-presentation/src/test/kotlin/com/goms/v2/domain/account/AccountControllerTest.kt
+++ goms-presentation/src/test/kotlin/com/goms/v2/domain/account/AccountControllerTest.kt
@@ -1,18 +1,23 @@
 package com.goms.v2.domain.account
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.goms.v2.domain.account.constant.Authority
 import com.goms.v2.domain.account.constant.Gender
 import com.goms.v2.domain.account.constant.Major
+import com.goms.v2.domain.account.data.dto.PasswordDto
 import com.goms.v2.domain.account.data.dto.ProfileDto
+import com.goms.v2.domain.account.dto.request.UpdatePasswordRequest
 import com.goms.v2.domain.account.dto.response.ProfileHttpResponse
 import com.goms.v2.domain.account.mapper.AccountDataMapper
 import com.goms.v2.domain.account.usecase.QueryAccountProfileUseCase
+import com.goms.v2.domain.account.usecase.UpdatePasswordUseCase
 import io.kotest.core.spec.style.DescribeSpec
 import io.mockk.every
 import io.mockk.mockk
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
@@ -23,9 +28,11 @@ class AccountControllerTest: DescribeSpec({
     lateinit var mockMvc: MockMvc
     val accountDataMapper = mockk<AccountDataMapper>()
     val queryAccountProfileUseCase = mockk<QueryAccountProfileUseCase>()
+    val updatePasswordUseCase = mockk<UpdatePasswordUseCase>()
     val accountController = AccountController(
         accountDataMapper,
-        queryAccountProfileUseCase
+        queryAccountProfileUseCase,
+        updatePasswordUseCase
     )
 
     beforeTest {
@@ -69,7 +76,7 @@ class AccountControllerTest: DescribeSpec({
                     .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
                     .andExpect(jsonPath("$.name").value(profileHttpResponse.name))
                     .andExpect(jsonPath("$.grade").value(profileHttpResponse.grade))
-                    .andExpect(jsonPath("$.gender").value(profileHttpResponse.gender))
+                    .andExpect(jsonPath("$.gender").value(profileHttpResponse.gender.toString()))
                     .andExpect(jsonPath("$.authority").value(profileHttpResponse.authority.toString()))
                     .andExpect(jsonPath("$.profileUrl").value(profileHttpResponse.profileUrl))
                     .andExpect(jsonPath("$.lateCount").value(profileHttpResponse.lateCount))
@@ -81,4 +88,33 @@ class AccountControllerTest: DescribeSpec({
 
     }
 
+
+    describe("api/v2/account/new-password patch 요청을 했을때") {
+        val url = "/api/v2/account/new-password"
+
+        val updatePasswordRequest = UpdatePasswordRequest(
+            email = "s22039@gsm.hs.kr",
+            newPassword = "gomstest1234!"
+        )
+        val passwordDto = PasswordDto(
+            email = "s22039@gsm.hs.kr",
+            newPassword = "gomstest1234!"
+        )
+
+        context("유효한 요청이 전달 되면") {
+
+            every { accountDataMapper.toDomain(updatePasswordRequest) } returns passwordDto
+            every { updatePasswordUseCase.execute(passwordDto) } returns Unit
+            val jsonRequestBody = jacksonObjectMapper().writeValueAsString(updatePasswordRequest)
+
+            it("204 status code를 응답해야한다.") {
+                mockMvc.perform(
+                    patch(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody)
+                )
+                    .andExpect(status().`is`(204))
+            }
+        }
+    }
 })
