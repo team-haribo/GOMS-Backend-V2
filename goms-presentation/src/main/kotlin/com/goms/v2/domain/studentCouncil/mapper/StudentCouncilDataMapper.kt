529e52c1af5be54153439c6011fe396867b51b19
Umjiseung <eom071005123@gmail.com>
2024-02-04 23:12:49 +0900

    ♻️::개행 수정

--- goms-presentation/src/main/kotlin/com/goms/v2/domain/studentCouncil/mapper/StudentCouncilDataMapper.kt
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/studentCouncil/mapper/StudentCouncilDataMapper.kt
@@ -67,7 +67,7 @@ class StudentCouncilDataMapper {
             major = lateAccountDto.major,
             profileUrl = lateAccountDto.profileUrl,
             createdTime = lateAccountDto.createdTime
-            )
+        )
 
 
 }
