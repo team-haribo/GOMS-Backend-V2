940066d5faa08c60d0cb38201f5bc05b1e99baa3
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-12 18:24:23 +0900

    ♻️ :: 상태 코드 반환 수정

--- goms-presentation/src/main/kotlin/com/goms/v2/domain/account/AccountController.kt
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/account/AccountController.kt
@@ -40,6 +40,6 @@ class AccountController(
     @PostMapping("image")
     fun uploadImage(@RequestPart("File") image: MultipartFile): ResponseEntity<Void> =
         uploadImageUseCase.execute(image)
-                .let { ResponseEntity.ok().build() }
+                .let { ResponseEntity.noContent().build() }
 
 }
