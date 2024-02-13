8eec22298f0b4a9d2f0539c51e30cc41b328aff9
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-02-13 21:26:38 +0900

    ✨ :: test 코드 추가

--- goms-presentation/src/test/kotlin/com/goms/v2/domain/account/AccountControllerTest.kt
+++ goms-presentation/src/test/kotlin/com/goms/v2/domain/account/AccountControllerTest.kt
@@ -11,6 +11,7 @@ import com.goms.v2.domain.account.dto.response.ProfileHttpResponse
 import com.goms.v2.domain.account.mapper.AccountDataMapper
 import com.goms.v2.domain.account.usecase.QueryAccountProfileUseCase
 import com.goms.v2.domain.account.usecase.UpdatePasswordUseCase
+import com.goms.v2.domain.account.usecase.UploadImageUseCase
 import io.kotest.core.spec.style.DescribeSpec
 import io.mockk.every
 import io.mockk.mockk
@@ -29,10 +30,12 @@ class AccountControllerTest: DescribeSpec({
     val accountDataMapper = mockk<AccountDataMapper>()
     val queryAccountProfileUseCase = mockk<QueryAccountProfileUseCase>()
     val updatePasswordUseCase = mockk<UpdatePasswordUseCase>()
+    val uploadImageUseCase = mockk<UploadImageUseCase>()
     val accountController = AccountController(
         accountDataMapper,
         queryAccountProfileUseCase,
-        updatePasswordUseCase
+        updatePasswordUseCase,
+        uploadImageUseCase
     )
 
     beforeTest {
