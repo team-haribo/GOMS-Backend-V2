82d7e7e602ebb288a0bbda21d4cebb3281223c90
KimGyeongsuuu <sk62628504@gmail.com>
2024-03-28 10:53:52 +0900

    :sparkles: :: delete device token api

--- goms-presentation/src/main/kotlin/com/goms/v2/domain/notification/NotificationController.kt
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/notification/NotificationController.kt
@@ -1,9 +1,11 @@
 package com.goms.v2.domain.notification
 
+import com.goms.v2.domain.notification.usecase.DeleteDeviceTokenUseCase
 import com.goms.v2.domain.notification.usecase.SendNotificationUseCase
 import com.goms.v2.domain.notification.usecase.SetDeviceTokenUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -14,13 +16,20 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("api/v2/notification")
 class NotificationController(
     private val setDeviceTokenUseCase: SetDeviceTokenUseCase,
-    private val sendNotificationUseCase: SendNotificationUseCase
+    private val sendNotificationUseCase: SendNotificationUseCase,
+    private val deleteDeviceTokenUseCase: DeleteDeviceTokenUseCase
 ) {
 
     @PostMapping("token/{deviceToken}")
     fun setDeviceToken(@PathVariable deviceToken: String): ResponseEntity<Void> =
         setDeviceTokenUseCase.execute(deviceToken)
             .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
+
+    @DeleteMapping("token")
+    fun deleteDeviceToken(): ResponseEntity<Void> =
+        deleteDeviceTokenUseCase.execute()
+            .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
+
 
     @GetMapping("outing/before")
     fun beforeOutingTestApi(): ResponseEntity<Map<String, String>> =
