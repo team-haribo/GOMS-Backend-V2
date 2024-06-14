0f298487b0d3a31adf0f7a01a50de8da5d60b7e3
KimGyeongsuuu <sk62628504@gmail.com>
2024-06-14 10:24:45 +0900

    update :: delete account late, outing info

--- goms-application/src/main/kotlin/com/goms/v2/domain/auth/usecase/WithdrawUseCase.kt
+++ goms-application/src/main/kotlin/com/goms/v2/domain/auth/usecase/WithdrawUseCase.kt
@@ -2,15 +2,18 @@ package com.goms.v2.domain.auth.usecase
 
 import com.goms.v2.common.annotation.UseCaseWithTransaction
 import com.goms.v2.common.util.AccountUtil
-import com.goms.v2.domain.auth.data.dto.WithdrawDto
 import com.goms.v2.domain.auth.exception.AccountNotFoundException
 import com.goms.v2.domain.auth.exception.PasswordNotMatchException
 import com.goms.v2.domain.auth.spi.PasswordEncoderPort
 import com.goms.v2.repository.account.AccountRepository
+import com.goms.v2.repository.late.LateRepository
+import com.goms.v2.repository.outing.OutingRepository
 
 @UseCaseWithTransaction
 class WithdrawUseCase(
     private val accountRepository: AccountRepository,
+    private val lateRepository: LateRepository,
+    private val outingRepository: OutingRepository,
     private val accountUtil: AccountUtil,
     private val passwordEncoderPort: PasswordEncoderPort
 ) {
@@ -24,6 +27,8 @@ class WithdrawUseCase(
             throw PasswordNotMatchException()
         }
 
+        lateRepository.deleteAllByAccountIdx(accountIdx)
+        outingRepository.deleteAllByAccountIdx(accountIdx)
         accountRepository.delete(account)
     }
 
