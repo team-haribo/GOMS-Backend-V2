87a7dacd4257519152103399b789d750c865ae63
KimGyeongsuuu <sk62628504@gmail.com>
2024-06-17 11:39:56 +0900

    add :: late ranking cache

--- goms-application/src/main/kotlin/com/goms/v2/domain/late/usecase/QueryLateRankUseCase.kt
+++ goms-application/src/main/kotlin/com/goms/v2/domain/late/usecase/QueryLateRankUseCase.kt
@@ -3,12 +3,18 @@ package com.goms.v2.domain.late.usecase
 import com.goms.v2.common.annotation.UseCaseWithReadOnlyTransaction
 import com.goms.v2.domain.late.data.dto.LateRankDto
 import com.goms.v2.repository.late.LateRepository
+import org.springframework.cache.annotation.Cacheable
 
 @UseCaseWithReadOnlyTransaction
 class QueryLateRankUseCase(
     private val lateRepository: LateRepository,
 ) {
 
+    @Cacheable(
+        value = ["RankList"],
+        key = "'rankList'",
+        cacheManager = "contentCacheManager",
+    )
     fun execute(): List<LateRankDto> {
         val lateRankList = lateRepository.findTop3ByOrderByAccountDesc()
         return lateRankList.map {
