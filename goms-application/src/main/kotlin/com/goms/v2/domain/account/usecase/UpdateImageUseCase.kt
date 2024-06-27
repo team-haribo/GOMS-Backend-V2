6872c19909765f1833aee9ae9a78e9e44dafe06c
KimGyeongsuuu <sk62628504@gmail.com>
2024-06-27 16:59:43 +0900

    feat :: user profile info caching

--- goms-application/src/main/kotlin/com/goms/v2/domain/account/usecase/UpdateImageUseCase.kt
+++ goms-application/src/main/kotlin/com/goms/v2/domain/account/usecase/UpdateImageUseCase.kt
@@ -1,17 +1,25 @@
 package com.goms.v2.domain.account.usecase
 
 import com.goms.v2.common.annotation.UseCaseWithTransaction
+import com.goms.v2.common.util.AccountUtil
 import com.goms.v2.domain.account.spi.S3UtilPort
 import com.goms.v2.domain.account.updateProfileUrl
 import com.goms.v2.repository.account.AccountRepository
+import org.springframework.cache.annotation.CacheEvict
 import org.springframework.web.multipart.MultipartFile
 
 @UseCaseWithTransaction
 class UpdateImageUseCase(
-        private val accountRepository: AccountRepository,
-        private val s3UtilPort: S3UtilPort,
+    private val accountRepository: AccountRepository,
+    private val s3UtilPort: S3UtilPort,
+    private val accountUtil: AccountUtil
 ) {
 
+    @CacheEvict(
+        value = ["userProfiles"],
+        key = "#root.target.generateCacheKey()",
+        cacheManager = "contentCacheManager"
+    )
     fun execute(image: MultipartFile) {
         val account = s3UtilPort.validImage(image)
         s3UtilPort.deleteImage(account.profileUrl.toString())
@@ -21,5 +29,9 @@ class UpdateImageUseCase(
         accountRepository.save(account)
 
     }
+
+    fun generateCacheKey(): String =
+        accountUtil.getCurrentAccountIdx().toString()
+
 
 }
