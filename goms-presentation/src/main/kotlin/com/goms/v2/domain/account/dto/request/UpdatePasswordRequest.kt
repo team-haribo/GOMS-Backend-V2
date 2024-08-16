d6f87b7e0b0dc5d41fe86bd635dcf3624d884f5c
KimGyeongsuuu <sk62628504@gmail.com>
2024-08-16 15:36:34 +0900

    add :: request dto validation

--- goms-presentation/src/main/kotlin/com/goms/v2/domain/account/dto/request/UpdatePasswordRequest.kt
+++ goms-presentation/src/main/kotlin/com/goms/v2/domain/account/dto/request/UpdatePasswordRequest.kt
@@ -1,6 +1,10 @@
 package com.goms.v2.domain.account.dto.request
 
+import javax.validation.constraints.NotBlank
+
 data class UpdatePasswordRequest(
+    @field:NotBlank
     val email: String,
+    @field:NotBlank
     val newPassword: String
 )
