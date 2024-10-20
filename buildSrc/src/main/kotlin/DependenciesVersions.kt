8a62645ab1d4c7f3a9ca0d7f3f8af9cb60f0533a
yoontaebin <gusckd000@gmail.com>
2024-10-20 23:30:54 +0900

    update :: kotest, mockk version

--- buildSrc/src/main/kotlin/DependenciesVersions.kt
+++ buildSrc/src/main/kotlin/DependenciesVersions.kt
@@ -1,9 +1,9 @@
 object DependenciesVersions {
     const val QUERYDSL = "5.0.0"
-    const val MOCKK = "1.12.0"
-    const val KOTEST_JUNIT = "5.3.2"
+    const val MOCKK = "1.13.8"
+    const val KOTEST_JUNIT = "5.4.2"
     const val KOTEST_EXTENSION = "1.1.2"
-    const val KOTEST_ASSERTIONS = "5.5.5"
+    const val KOTEST_ASSERTIONS = "5.4.2"
     const val SPRING_TRANSACTION = "5.3.11"
     const val JACKSON_CORE = "2.13.4"
     const val SPRING_TEST = "2.7.7"
