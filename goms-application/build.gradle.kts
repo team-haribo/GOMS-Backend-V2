e86d3075a554069b09310a82fc5c702fc6b4baf0
yoontaebin <gusckd000@gmail.com>
2024-10-20 23:33:40 +0900

    add :: jacoco setting

--- goms-application/build.gradle.kts
+++ goms-application/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("plugin.allopen") version PluginVersions.ALL_OPEN_VERSION
+    id("jacoco")
 }
 
 repositories {
@@ -24,4 +25,38 @@ dependencies {
 allOpen {
     annotation(AllOpen.USECASE_WIHT_TRANSACTION)
     annotation(AllOpen.USECASE_WIHT_READONLY_TRANSACTION)
+}
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+tasks.test {
+    finalizedBy("jacocoTestReport") // 테스트 후에 JaCoCo 리포트 생성
+
+    jacoco {
+        isEnabled = true // JaCoCo가 활성화되어 있는지 확인
+    }
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // 테스트 후에 JaCoCo 리포트 생성
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+
+    classDirectories.setFrom(
+        files(classDirectories.files.map {
+            fileTree(it) {
+                exclude("**/common/**")
+                exclude("**/data/**")
+                exclude("**/exception/**")
+                exclude("**/scheduler/**")
+                exclude("**/spi/**")
+                exclude("**/event/**")
+            }
+        })
+    )
 }
