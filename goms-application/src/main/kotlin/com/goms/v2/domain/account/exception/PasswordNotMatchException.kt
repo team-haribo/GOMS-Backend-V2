e52b665a7aea177249a0f4875f1ab10bb99eaea9
snowykte0426 <snowykte0426@naver.com>
2025-07-22 19:05:53 +0900

    🔥 :: 중복 예외 클래스 제거

--- goms-application/src/main/kotlin/com/goms/v2/domain/account/exception/PasswordNotMatchException.kt
+++
@@ -1,6 +0,0 @@
-package com.goms.v2.domain.account.exception
-
-import com.goms.v2.common.exception.ErrorCode
-import com.goms.v2.common.exception.GomsException
-
-class PasswordNotMatchException: GomsException(ErrorCode.PASSWORD_NOT_MATCH)
