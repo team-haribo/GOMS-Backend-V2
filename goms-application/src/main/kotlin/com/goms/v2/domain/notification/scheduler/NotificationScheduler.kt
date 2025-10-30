3072f90a8e4cbd5b423d89e76f3431f5be174421
Kim Tae Eun <snowykte0426@naver.com>
2025-10-30 09:03:28 +0900

    🔥 :: AWS S3로 로그 전송 및 정기 푸쉬 알람 전송 스케쥴러 비활성화 (#361)

--- goms-application/src/main/kotlin/com/goms/v2/domain/notification/scheduler/NotificationScheduler.kt
+++ goms-application/src/main/kotlin/com/goms/v2/domain/notification/scheduler/NotificationScheduler.kt
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Component
 class NotificationScheduler(
     private val sendNotificationUseCase: SendNotificationUseCase
 ) {
-    @Scheduled(cron = "0 30 17 ? * 1,3") // 매주 월요일, 수요일 5시 30분에 미리 공지한다. (1시간 10분 전)
-    fun sendFirstNotification() =
-        sendNotificationUseCase.execute(NotificationType.FIRST_NOTIFICATION)
-
-    @Scheduled(cron = "0 35 18 ? * 1,3") // 매주 월요일, 수요일 6시 35분에 공지한다. (5분 전)
-    fun sendSecondNotification() =
-        sendNotificationUseCase.execute(NotificationType.FINAL_NOTIFICATION)
+//    @Scheduled(cron = "0 30 17 ? * 1,3") // 매주 월요일, 수요일 5시 30분에 미리 공지한다. (1시간 10분 전)
+//    fun sendFirstNotification() =
+//        sendNotificationUseCase.execute(NotificationType.FIRST_NOTIFICATION)
+//
+//    @Scheduled(cron = "0 35 18 ? * 1,3") // 매주 월요일, 수요일 6시 35분에 공지한다. (5분 전)
+//    fun sendSecondNotification() =
+//        sendNotificationUseCase.execute(NotificationType.FINAL_NOTIFICATION)
 }
